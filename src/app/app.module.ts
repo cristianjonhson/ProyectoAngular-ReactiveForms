5ea19eab9d9fdabe847150e25a2047fd1ebc2d7e
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-08-05 12:57:54 -0400

    feat: agregar módulo de animaciones de Angular al módulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { UsersModule } from './modules/users/users.module';
@@ -10,6 +11,7 @@ import { UsersModule } from './modules/users/users.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     UsersModule
   ],
   providers: [],
